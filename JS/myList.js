b4d06a4eef36ce14cebdf197ca36982b4ea73225
Alon Okun <alon.okun@gokstadakademiet.no>
2024-04-10 13:12:00 +0200

    Ingen endringer

--- JS/myList.js
+++ JS/myList.js
@@ -1,101 +1,103 @@
-const deleteData = (object) => {
-  let watchList = JSON.parse(localStorage.getItem("data"));
-  console.log(watchList);
-  let index = watchList.findIndex((movie) => movie.title === object.title);
-  console.log(index);
-  watchList.splice(index, 1);
-  localStorage.setItem("data", JSON.stringify(watchList));
-
-  showData();
-};
-const myWatchList = document.querySelector("#myWatchList");
-const showData = () => {
-  myWatchList.innerHTML = "";
-  let data = JSON.parse(localStorage.getItem("data"));
-
-  data.forEach((movie) => {
-    showMyMovies(movie);
-  });
-};
-
-const showMyMovies = (movie) => {
-  let container = document.createElement("li");
-  container.style.display = "flex";
-  container.style.gap = "10px";
-  container.style.alignItems = "center";
-
-  let confirmButton = document.createElement("button");
-  confirmButton.innerHTML = `<i class="fa-regular fa-circle"></i>`;
-  confirmButton.style.border = "none";
-  confirmButton.style.outline = "none";
-  confirmButton.style.background = "none";
-  confirmButton.style.fontSize = "2rem";
-  container.appendChild(confirmButton);
-
-  let movieInformation = document.createElement("div");
-  movieInformation.style.width = "100%";
-  movieInformation.style.border = "1px solid black";
-  movieInformation.style.display = "flex";
-  movieInformation.style.justifyContent = "space-between";
-  movieInformation.style.alignItems = "center";
-
-  let informationBox = document.createElement("div");
-  informationBox.style.display = "flex";
-  informationBox.style.alignItems = "center";
-  informationBox.style.gap = "10px";
-  let image = document.createElement("img");
-
-  image.alt = `${movie.title} cover`;
-  image.onerror = () => {
-    this.src =
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png";
-  };
-  image.style.height = "300px";
-  image.style.width = "150px";
-  image.style.objectFit = "cover";
-  if (!movie.thumbnail) {
-    image.src =
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png";
-  } else {
-    image.src = movie.thumbnail;
+const deleteData= (object)=>{
+    let watchList=JSON.parse(localStorage.getItem('data'));
+    console.log(watchList)
+    let index= watchList.findIndex(movie=>movie.title===object.title);
+    console.log(index)
+    watchList.splice(index,1);
+    localStorage.setItem("data", JSON.stringify(watchList))
+    
+    showData()
   }
-  informationBox.appendChild(image);
-
-  let textbox = document.createElement("div");
-  let title = document.createElement("h2");
-  let year = document.createElement("p");
-  title.innerHTML = movie.title;
-  year.innerHTML = movie.year;
-  title.style.fontSize = "2rem";
-
-  textbox.appendChild(title);
-  textbox.appendChild(year);
-
-  let removeButton = document.createElement("button");
-  removeButton.innerHTML = `<i class="fa-solid fa-pen"></i>`;
-  removeButton.style.border = "none";
-  removeButton.style.outline = "none";
-  removeButton.style.background = "none";
-  removeButton.style.fontSize = "2rem";
-  removeButton.style.paddingRight = "10px";
-  removeButton.addEventListener("click", () => {
-    confirmButton.innerHTML = `<i class="fa-solid fa-trash-can"></i>`;
-    removeButton.innerHTML = `<i class="fa-solid fa-check"></i>`;
-
-    confirmButton.addEventListener("click", () => {
-      deleteData(movie);
+  const myWatchList= document.querySelector("#myWatchList")
+  const showData=()=>{
+    myWatchList.innerHTML=""
+    let data= JSON.parse(localStorage.getItem("data"))
+    
+    data.forEach(movie => {
+        showMyMovies(movie)
     });
-    removeButton.addEventListener("click", () => {
-      removeButton.innerHTML = `<i class="fa-solid fa-pen"></i>`;
-      confirmButton.innerHTML = `<i class="fa-regular fa-circle"></i>`;
-    });
-  });
+  }
 
-  informationBox.appendChild(textbox);
-  movieInformation.appendChild(informationBox);
-  movieInformation.appendChild(removeButton);
-  container.appendChild(movieInformation);
+const showMyMovies= (movie)=>{
+    
+    let container=document.createElement("li");
+    container.style.display="flex"
+    container.style.gap="10px";
+    container.style.alignItems="center"
+    
+    let confirmButton=document.createElement("button");
+    confirmButton.innerHTML=`<i class="fa-regular fa-circle"></i>`
+    confirmButton.style.border="none"
+    confirmButton.style.outline="none"
+    confirmButton.style.background="none"
+    confirmButton.style.fontSize="2rem"
+    container.appendChild(confirmButton);
 
-  myWatchList.appendChild(container);
-};
-showData();
+    let movieInformation= document.createElement("div");
+    movieInformation.style.width="100%"
+    movieInformation.style.border="1px solid black"
+    movieInformation.style.display="flex"
+    movieInformation.style.justifyContent="space-between";
+    movieInformation.style.alignItems="center"
+    
+    let informationBox= document.createElement("div");
+    informationBox.style.display="flex"
+    informationBox.style.alignItems="center"
+    informationBox.style.gap="10px"
+    let image= document.createElement("img")
+    
+    image.alt=`${movie.title} cover`;
+    image.onerror=()=> {this.src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png"}
+    image.style.height="300px"
+    image.style.width="150px"
+    image.style.objectFit="cover"
+    if(!movie.thumbnail){
+      image.src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png"
+      
+    }else{
+      image.src=movie.thumbnail;
+    }
+    informationBox.appendChild(image);
+    
+    let textbox=document.createElement("div");
+    let title= document.createElement("h2");
+    let year= document.createElement("p");
+    title.innerHTML=movie.title;
+    year.innerHTML=movie.year;
+    title.style.fontSize="2rem"
+    
+    textbox.appendChild(title)
+    textbox.appendChild(year)
+    
+    let removeButton= document.createElement("button");
+    removeButton.innerHTML=`<i class="fa-solid fa-pen"></i>`
+    removeButton.style.border="none"
+    removeButton.style.outline="none"
+    removeButton.style.background="none"
+    removeButton.style.fontSize="2rem"
+    removeButton.style.paddingRight="10px"
+    removeButton.addEventListener("click", ()=>{
+        confirmButton.innerHTML=`<i class="fa-solid fa-trash-can"></i>`
+        removeButton.innerHTML=`<i class="fa-solid fa-check"></i>`
+        
+        confirmButton.addEventListener("click", ()=>{
+        deleteData(movie)
+        })
+        removeButton.addEventListener("click", ()=>{
+            removeButton.innerHTML=`<i class="fa-solid fa-pen"></i>`
+            confirmButton.innerHTML=`<i class="fa-regular fa-circle"></i>`
+            
+        })
+    })
+
+    informationBox.appendChild(textbox);
+    movieInformation.appendChild(informationBox)
+    movieInformation.appendChild(removeButton)
+    container.appendChild(movieInformation)
+    
+   
+
+    myWatchList.appendChild(container)
+    
+  }
+  showData()
--- JS/script.js
+++ JS/script.js
@@ -8,12 +8,13 @@ const fetchMovies = async () => {
     const res = await fetch(BASE_URL);
     if (!res.ok) {
       throw new Error("Ops, noe gikk feil med hentingen.");
+
     }
     const data = await res.json();
     allMovies = data;
-    sortInAlphabeticalOrder().forEach((movie) => {
-      showMovies(movie);
-    });
+    sortInAlphabeticalOrder().forEach(movie => {
+            showMovies(movie);
+        });
   } catch (error) {
     console.error(error);
   }
@@ -23,153 +24,151 @@ fetchMovies();
 
 //Fetch movielist-div
 const movielistContainer = document.getElementById("movielistContainer");
-const myWatchList = document.querySelector("#myWatchList");
+const myWatchList= document.querySelector("#myWatchList");
 
 //Show movies
 const showMovies = (movie) => {
-  const divContainer = document.createElement("div");
-  divContainer.addEventListener("click", () => {
-    addToWatchList(movie);
-  });
-  const divTitleContainer = document.createElement("div");
-  const image = document.createElement("img");
-  const titleText = document.createElement("p");
-  const yearText = document.createElement("p");
-  divContainer.style.display = "flex";
-  divContainer.style.alignItems = "center";
-  divContainer.style.justifyContent = "space-between";
-  divContainer.style.flexFlow = "column wrap";
-  divContainer.style.border = "2px solid gray";
-  divContainer.style.borderRadius = "5px";
-  divTitleContainer.style.display = "flex";
-  divTitleContainer.style.width = "180px";
-  divTitleContainer.style.justifyContent = "center";
-  divTitleContainer.style.textAlign = "center";
-
-  if (!movie.thumbnail) {
-    image.src =
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png";
-  } else {
-    image.src = movie.thumbnail;
-  }
-  image.alt = movie.title + "-cover";
-  image.style.height = "300px";
-  image.style.width = "200px";
-  image.style.objectFit = "cover";
-  image.onerror = () => {
-    image.src =
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png";
-  };
-  titleText.innerHTML = movie.title;
-  titleText.style.fontFamily = "Mongolian Baiti, Times New Roman, serif";
-  titleText.style.fontSize = "1.7rem";
-  titleText.style.margin = "10px 0";
-  yearText.innerHTML = movie.year;
-  yearText.style.fontSize = "1rem";
-  yearText.style.marginBottom = "10px";
-  yearText.style.color = "#595959";
-  divContainer.appendChild(image);
-  divTitleContainer.appendChild(titleText);
-  divContainer.appendChild(divTitleContainer);
-  divContainer.appendChild(yearText);
-  movielistContainer.appendChild(divContainer);
-};
+    const divContainer = document.createElement("div");
+    divContainer.addEventListener("click", ()=>{
+      addToWatchList(movie)
+    })
+    const divTitleContainer = document.createElement("div");
+    const image = document.createElement("img");
+    const titleText = document.createElement("p");
+    const yearText = document.createElement("p");
+    divContainer.style.display = "flex";
+    divContainer.style.alignItems = "center";
+    divContainer.style.justifyContent = "space-between";
+    divContainer.style.flexFlow = "column wrap";
+    divContainer.style.border = "2px solid gray";
+    divContainer.style.borderRadius = "5px";
+    divTitleContainer.style.display = "flex";
+    divTitleContainer.style.width = "180px";
+    divTitleContainer.style.justifyContent = "center";
+    divTitleContainer.style.textAlign = "center";
+   
+    if (!movie.thumbnail) {
+        image.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png";
+    } else {
+        image.src = movie.thumbnail;
+    }
+    image.alt = movie.title + "-cover";
+    image.style.height = "300px";
+    image.style.width = "200px";
+    image.style.objectFit = "cover";
+    image.onerror=() => {
+      image.src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png"
+    }
+    titleText.innerHTML = movie.title;
+    titleText.style.fontFamily = "Mongolian Baiti, Times New Roman, serif";
+    titleText.style.fontSize = "1.7rem";
+    titleText.style.margin = "10px 0";
+    yearText.innerHTML = movie.year;
+    yearText.style.fontSize = "1rem";
+    yearText.style.marginBottom = "10px";
+    yearText.style.color = "#595959";
+    divContainer.appendChild(image);
+    divTitleContainer.appendChild(titleText);
+    divContainer.appendChild(divTitleContainer)
+    divContainer.appendChild(yearText);
+    movielistContainer.appendChild(divContainer);
+}
 
 //Filter release year
 const selectYearsForm = document.getElementById("selectYearsForm");
 selectYearsForm.style.display = "none";
 let i = 0;
 const showYearSelection = () => {
-  if (selectYearsForm.style.display == "none") {
-    if (i === 0) {
+   if (selectYearsForm.style.display == "none") {
+    if (i === 0){
       showReleaseYear();
       i += 1;
     }
     selectYearsForm.style.display = "flex";
-  } else {
+   } else {
     selectYearsForm.style.display = "none";
-  }
+   }
 };
 
 //Sort movie release year
-const showReleaseYear = () => {
-  const allYears = allMovies.map((movie) => movie.year);
-  let uniqueYears = Array.from(new Set(allYears));
-  uniqueYears.sort();
-  uniqueYears.forEach((year) => {
-    displayReleaseYear(year);
-  });
-};
+const showReleaseYear=()=>{
+    const allYears = allMovies.map(movie=>movie.year);
+    let uniqueYears = Array.from(new Set(allYears))
+    uniqueYears.sort();
+    uniqueYears.forEach(year => {
+        displayReleaseYear(year);
+    });
+}
 
 const displayReleaseYear = (year) => {
-  const yearContainer = document.createElement("div");
-  const yearInput = document.createElement("input");
-  const yearLabel = document.createElement("label");
-  yearContainer.classList.add("yearContainer");
-  yearInput.type = "radio";
-  yearInput.id = "year" + year;
-  yearInput.name = "year";
-  yearInput.value = year;
-  yearInput.addEventListener("click", () => {
-    const yearRadioBtn = document.getElementById(`year${year}`);
-    if (yearRadioBtn.checked == true) {
-      let filteredMovies = chooseReleaseYear(yearRadioBtn.value);
-      movielistContainer.innerHTML = "";
-      filteredMovies.forEach((movie) => showMovies(movie));
-    }
-  });
-  yearInput.classList.add("radio");
-  yearLabel.for = "year" + year;
-  yearLabel.innerHTML = year;
-  yearContainer.appendChild(yearInput);
-  yearContainer.appendChild(yearLabel);
-  selectYearsForm.appendChild(yearContainer);
-};
+    const yearContainer = document.createElement("div");
+    const yearInput = document.createElement("input");
+    const yearLabel = document.createElement("label");
+    yearContainer.classList.add("yearContainer");
+    yearInput.type = "radio";
+    yearInput.id = "year" + year;
+    yearInput.name = "year";
+    yearInput.value = year;
+    yearInput.addEventListener("click", () => {
+        const yearRadioBtn = document.getElementById(`year${year}`);
+        if (yearRadioBtn.checked == true) {
+          let filteredMovies = chooseReleaseYear(yearRadioBtn.value);
+          movielistContainer.innerHTML = "";
+          filteredMovies.forEach(movie => showMovies(movie))
+        }
+    });
+    yearInput.classList.add("radio");
+    yearLabel.for = "year" + year;
+    yearLabel.innerHTML = year;
+    yearContainer.appendChild(yearInput);
+    yearContainer.appendChild(yearLabel);
+    selectYearsForm.appendChild(yearContainer);
+}
 
 const yearAllBtn = document.getElementById("yearAllBtn");
 yearAllBtn.addEventListener("click", () => {
-  if (yearAllBtn.checked == true) {
-    movielistContainer.innerHTML = "";
-    allMovies.forEach((movie) => showMovies(movie));
-  }
+    if (yearAllBtn.checked == true) {
+        movielistContainer.innerHTML = "";
+        allMovies.forEach(movie => showMovies(movie));
+    }
 });
 
-const chooseReleaseYear = (value) => {
+const chooseReleaseYear=(value)=>{
   value = Number(value);
-  return allMovies.filter((movie) => movie.year === value);
-};
+  return allMovies.filter(movie=>movie.year===value)
+}
 
 // Filter Genre
 const selectGenresForm = document.getElementById("selectGenresForm");
 selectGenresForm.style.display = "none";
 const showGenreSelection = () => {
-  if (selectGenresForm.style.display == "none") {
+   if (selectGenresForm.style.display == "none") {
     selectGenresForm.style.display = "flex";
-  } else {
+   } else {
     selectGenresForm.style.display = "none";
-  }
+   }
 };
 
-//Choose movie genre
-const allMovieGenre = () => {
-  const findGenres = allMovies.flatMap((movie) => movie.genres);
-  let allGenres = Array.from(new Set(findGenres));
-  return allGenres;
-};
+//Choose movie genre 
+const allMovieGenre= ()=>{
+    const findGenres= allMovies.flatMap(movie =>  movie.genres)
+    let allGenres=Array.from(new Set(findGenres))
+    return allGenres
+}
 
-const chooseMovieGenre = (value) => {
-  let choosenGenre = allMovies.filter((movie) => {
-    return movie.genres.includes(value);
-  });
-  return choosenGenre;
-};
+const chooseMovieGenre= (value)=>{
+    let choosenGenre= allMovies.filter(movie=>{
+        return movie.genres.includes(value)
+    })
+    return choosenGenre
+}
 
 //Fetch inputtypes
 const findMovieInput = document.querySelector("#findMovieInput");
 const rangeYearInput = document.querySelector("#rangeYear");
 
 //Fetch overlay
-const overlay = document.querySelector("#overlay");
+const overlay = document.querySelector('#overlay');
 
 //Fetch knapper
 const randomMovieBtn = document.querySelector("#randomMovieBtn");
@@ -181,56 +180,63 @@ const sortMovieGenre = document.querySelector("#sortMovieGenre");
 const rangeYearData = document.querySelector("#rangeYearData");
 
 //Find movie
-const findMovie = () => {
-  const inputValue = findMovieInput.value.toLowerCase();
-  const foundMovies = allMovies.filter((movie) =>
-    movie.title.toLowerCase().includes(inputValue)
-  );
+    const findMovie = () => {
+        const inputValue = findMovieInput.value.toLowerCase();
+        const foundMovies = allMovies.filter(
+          (movie) => movie.title.toLowerCase().includes(inputValue)
+        );
+      
+        
+      
+        if (foundMovies.length > 0) {
+          movielistContainer.innerHTML = "";
+          foundMovies.forEach(movie => {
+            showMovies(movie);
+          });
+        }
+      };
 
-  if (foundMovies.length > 0) {
-    movielistContainer.innerHTML = "";
-    foundMovies.forEach((movie) => {
-      showMovies(movie);
-    });
-  }
-};
-
-findMovieInput.addEventListener("input", findMovie);
+      
+      findMovieInput.addEventListener("input", findMovie);
+      
+      
 
 //Add movie to movie library
 
 //Choose random movie function
-const randomMovie = () => {
-  const randomNumber = Math.floor(Math.random() * allMovies.length);
-  return allMovies[randomNumber];
-};
-randomMovieBtn.addEventListener("click", () => {
-  console.log(randomMovie());
-});
+const randomMovie= ()=>{
+    const randomNumber= Math.floor(Math.random()*allMovies.length);
+    return allMovies[randomNumber]
+}
+randomMovieBtn.addEventListener("click", ()=>{
+console.log(randomMovie())
+})
 
 //Local storage
 const saveData = () => {
-  if (localStorage.getItem("data")) {
-    return;
-  } else {
-    localStorage.setItem("data", JSON.stringify([])); //Lagrer innhold i movielist
+  if(localStorage.getItem("data")){
+    return
   }
-};
-const addToWatchList = (object) => {
-  let watchList = JSON.parse(localStorage.getItem("data"));
-  const checkIfExist = watchList.some((movie) => movie.title === object.title);
-  console.log(checkIfExist);
-  if (!checkIfExist) {
-    watchList.push(object);
-    localStorage.setItem("data", JSON.stringify(watchList));
-  } else {
-    console.log("Den finns redan");
+  else{
+    localStorage.setItem('data', JSON.stringify([])); //Lagrer innhold i movielist
   }
-};
+}
+const addToWatchList=(object)=>{
+  let watchList= JSON.parse(localStorage.getItem("data"))
+  const checkIfExist = watchList.some(movie => movie.title === object.title)
+  console.log(checkIfExist)
+  if(!checkIfExist){
+    watchList.push(object)
+    localStorage.setItem('data', JSON.stringify(watchList))
+  }else{
+    console.log("Den finns redan")
+  }
+  
+}
 
 saveData();
 
-//Sort movie by letter in the alphabet
-const sortInAlphabeticalOrder = () => {
-  return allMovies.sort((a, b) => a.title.localeCompare(b.title));
-};
+//Sort movie by letter in the alphabet 
+const sortInAlphabeticalOrder= ()=>{
+return allMovies.sort((a,b)=> a.title.localeCompare(b.title))
+}
