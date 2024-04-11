c5800cf829cf32b9baaa2ce8f2c4aded12ec6c33
Glenn Pedersen <glenn.pedersen@gokstadakademiet.no>
2024-04-11 12:34:10 +0200

    Opprettet javascript filer til login og registerere ny bruker. Endret src. i html til de to nevnte sidene. Fjernet fetch til

--- JS/script.js
+++ JS/script.js
@@ -3,9 +3,7 @@
 const BASE_URL =
   "https://raw.githubusercontent.com/prust/wikipedia-movie-data/master/movies-2020s.json";
 
-const USERBASE_URL= "https://crudapi.co.uk/api/v1/probe"
-const API_KEY= "XgZAbMaJMOh5JZMo8gqNs8I__snYynl3o_H7dtDrhIfBClHGcQ"
-const headers= {"Authorization": `Bearer ${API_KEY}`};
+
 let allMovies;
 const fetchMovies = async () => {
   try {
@@ -364,67 +362,3 @@ const showSpecificMovie = (movie) => {
   });
 };
 
-
-
-//Create newUser 
-
-//Login functions and sessionstorage setup
-const setLoginstatus = (status) => {
-  sessionStorage.setItem("loggedIn", status ? "true" : "false");
-};
-const loggedIn = () => {
-  return sessionStorage.getItem("loggedIn") === "true";
-}; 
-const getLoggedInUser= ()=>{
-  return JSON.parse(sessionStorage.getItem("loggedInUser"));
- }
- const setLoggedInUser=(id)=>{
-  return sessionStorage.setItem("loggedInUser", JSON.stringify(id))
- }
- 
- //Verification username and login
- const ifUsernameExist = async(username)=>{
-  try{
-    const res= await fetch(USERBASE_URL);
-    if(!res.ok){
-      throw new Error("Något er feil i databasen ved verifiering av bruker");
-    }
-    const data=await res.json();
-    return data.some(user=>user.username===username)
-  }catch(error){
-    console.error("Något blev fel i verifiering av brukernavn")
-  }
- }
- const verifyLogin= async(username, password)=>{
- try{
-  const res= await fetch(USERBASE_URL);
-  if(!res.ok){
-    throw new Error("Något blev fel i fetch til verifiering av login")
-  }
-  const data= await res.json();
-  return data.some(user=> user.username===username && user.password===password)
-
- }catch(error){
-  console.error("Kunde inte verifiera login", error)
- }
- }
-
-
-
- const userLogin = async()=>{
-  const usernameInput= document.querySelector("#userNameInput").value;
-  const passwordInput= document.querySelector("passwordInput").value;
-  try{
-   if(!await verifyLogin(usernameInput, passwordInput)){
-    console.log("Feil brukernavn/passord")
-   }else{
-    setLoginstatus(true);
-   const userID= await fetchUserID(usernameInput);
-   sessionStorage.setItem("loggedInUser", JSON.stringify(userID));
-   
-   userLogin()
-  }
-  }catch(error){
-    console.error("Något blev feil i verifiering av login")
-  }
- }
